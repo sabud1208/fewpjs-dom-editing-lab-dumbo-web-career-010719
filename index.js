8c3be267580ea9400ef0052691a3dd122d4c5734
Stephanie Abud <sabud1208@gmail.com>
2019-02-19 00:52:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,10 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Introduction to the DOM Lab</title>
   <h1>My HTML adventure</h1>
-
+<p>
+  We're writing HTML markup to display in our <strong>browser</strong>.
+  We're basically telling computers what to do. <em>Neat!</em>
+</p>
 </head>
 <body>
   <!--All our work for this lesson will go here-->
